4f9843431ac6b92490f24315700fda2693227b02
Emily Ho <emilyy.ho@mail.utoronto.ca>
2026-07-09 23:44:30 -0400

    Adding a projects page

--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -1,9 +1,80 @@
+import healthcareFront from "../assets/images/healthcare_app/healthcare_front.jpg";
+import thankyouCCC from "../assets/images/robotic_arm/thankyou_ccc.jpg";
+import assemblyMainMenu from "../assets/images/assembly_project/main_menu.png";
+import systemMonitoring from "../assets/images/sysMonTool.png";
+import utt from "../assets/images/android_app/UTT.png";
+import gameCarnival from "../assets/images/game_carnival/game_carnival.png";
+import citationMachine from "../assets/images/citation_machine.png";
+import stocksGraph from "../assets/images/stocks_app/stocks_graph.png";
+import ai_sales_home_page from "../assets/images/ai_sales_app/home_page.png";
+import moodifyLogin from "../assets/images/moodify_app/login_page.png";
+
 const projects = [
     {
-        title: "Personal Website",
-        description: "A personal website built with React and Tailwind CSS to showcase my projects, experience, and hobbies.",
-        image: "/images/projects/personal_website.png",
-        link: ""
+        title: "Emotion Detection Music App",
+        description: "This music app uses AI, facial detection, and recommendation algorithms to recommend songs based on your mood to create a personalized and interactive music experience. Users can listen to music, share playlists with friends, and have voice / video calls.",
+        image: moodifyLogin,
+        githubLink: ""
+    },
+    {
+        title: "Operating System",
+        description: "Created an operating system by implementing thread synchronization, system calls, virtual memory, and the file system.",
+        image: "",
+        githubLink: ""
+    },
+    {
+        title: "AI Sales App",
+        description: "This web app uses AI to find potential clients that would be the best fit for the services that your company offers.",
+        image: ai_sales_home_page,
+        githubLink: ""
+    },
+    {
+        title: "Stock Management App",
+        description: "This web app allows users to manage their stocks, see future stock prices, perform transactions, and connect with friends.",
+        image: stocksGraph,
+        githubLink: ""
+    },
+    {
+        title: "Healthcare Web Application",
+        description: "A healthcare web application built with React and Tailwind CSS to improve patient care and streamline medical workflows.",
+        image: healthcareFront,
+        githubLink: ""
+    },
+    {
+        title: "Robotic Arm Drawing Automation",
+        description: "This project gets the user to draw something (simple or very detailed) on the screen, and then the robotic arms will draw that exact image on paper or makeup.",
+        image: thankyouCCC,
+        githubLink: ""
+    },
+    {
+        title: "Platform Game: Angry Birds Coin Quest",
+        description: "This platform game based on the game Angry Birds is built using Assembly language for the MIPS processor.",
+        image: assemblyMainMenu,
+        githubLink: ""
+    },
+    {
+        title: "System Monitoring Tool",
+        description: "This system monitoring tool developed in C for Unix/Linux environments provides real-time insights into system performance metrics.",
+        image: systemMonitoring,
+        githubLink: ""
+    },
+    {
+        title: "Android App: University Timetable Generator",
+        description: "This Android app is aimed to help students automatically plan and forecast courses required to graduate.",
+        image: utt,
+        githubLink: ""
+    },
+    {
+        title: "Game Carnival",
+        description: "This is an interactive and educational game for children, developed using Python, Pygame, software design patterns, and object-oriented programming.",
+        image: gameCarnival,
+        githubLink: ""
+    },
+    {
+        title: "Citation Machine",
+        description: " Delivered a Java program that helps students cite their sources in the requested MLA, APA, or Chicago format, effectively mitigating the risk of plagiarism.",
+        image: citationMachine,
+        githubLink: ""
     }
 ]
 
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import ContentContainer from '../components/ContentContainer'
+import projects from '../data/projects'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <ContentContainer>
+      <h1 className='text-white text-2xl'>PROJECTS</h1>
+      <div className='min-h-screen'>
+        <div className='grid grid-cols-4 gap-4 mt-4'>
+          {projects.map((project, index) => (
+            <button key={index} className='relative overflow-hidden bg-neutral-700/80 rounded-lg text-left'>
+              <img src={project.image} alt={project.title} className='w-full h-56 object-cover' />
+              <div className='absolute inset-0 bg-gradient-to-t from-black/80 via-black/20 to-transparent'/>
+              <h2 className='absolute bottom-0 left-0 p-4 text-white text-lg font-bold'>{project.title}</h2>
+            </button>
+          ))}
+        </div>
+      </div>
+    </ContentContainer>
   )
 }
 
